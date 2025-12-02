3108d5917ff5f2d2238648c07fc91a24476b0343
Queralt Martínez <xanti.pod@gmail.com>
2025-12-02 15:48:40 +0100

    build: fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/qmf-portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/qmf-portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
